69dd905666d4d64b32c16eae8d398cee5f8fc958
Indrajeet <72642249+iamindrajeet@users.noreply.github.com>
2024-08-15 13:17:19 +0530

    Time: 119 ms (100%), Space: 42.3 MB (37.51%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -2,43 +2,53 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int len = nums.length;
 
-        // If the array has only one element, return its index
+        // If the array has only one element, that element is the peak.
         if (len == 1)
             return 0;
 
-        // If the first element is greater than the second, return the index 0
+        // Check if the first element is the peak.
         if (nums[0] > nums[1])
             return 0;
 
-        // If the last element is greater than the second last, return the index len - 1
+        // Check if the last element is the peak.
         if (nums[len - 1] > nums[len - 2])
             return len - 1;
 
-        // Initialize the binary search bounds
-        int left = 1, right = len - 2;
+        // Initialize the binary search boundaries.
+        int low = 1, high = len - 2;
 
-        // Perform binary search
-        while (left <= right) {
-            // Find the middle index to avoid overflow
-            int mid = left + (right - left) / 2;
+        // Perform binary search.
+        while (low <= high) {
+            // Find the middle index.
+            int mid = low + (high - low) / 2;
 
-            // Check if the mid element is greater than its neighbors
+            // If the middle element is greater than its neighbors, it is the peak.
             if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
                 return mid;
-
-            // If the mid element is greater than the element to its left,
-            // then the peak must be in the right half
+            
+            // If the middle element is greater than the previous element,
+            // move to the right half.
             else if (nums[mid] > nums[mid - 1])
-                left = mid + 1;
-
-            // If the mid element is not greater than the element to its left,
-            // then the peak must be in the left half
+                low = mid + 1;
+            
+            // Otherwise, move to the left half.
             else
-                right = mid - 1;
+                high = mid - 1;
         }
 
-        // If no peak is found, return -1 (although the problem guarantees there will be
-        // a peak)
+        // Return -1 if no peak is found.
         return -1;
     }
 }
+
+/*
+ * Time Complexity (TC):
+ * The binary search approach divides the search space in half at each step,
+ * making the time complexity O(log n), where n is the number of elements
+ * in the input array.
+ *
+ * Space Complexity (SC):
+ * The space complexity is O(1) since we are only using a few extra variables
+ * for the binary search process and not using any additional space proportional
+ * to the input size.
+ */
