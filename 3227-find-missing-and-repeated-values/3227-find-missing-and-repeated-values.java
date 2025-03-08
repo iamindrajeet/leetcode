0f21d49a9d4a96730da824d5b04482f0766656b8
Indrajeet <72642249+iamindrajeet@users.noreply.github.com>
2025-03-08 20:31:15 +0530

    Time: 6 ms (25.12%), Space: 45.8 MB (26.23%) - LeetHub

---
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -0,0 +1,28 @@
+/**
+Approach : Using HashMap
+T.C - O(N^2)
+S.C - O(N^2)
+*/
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int missing = -1, repeat = -1;
+
+        // store the frequency of each number in the grid
+        Map<Integer, Integer> freq = new HashMap<>();
+        for(int[] row : grid) {
+            for(int num : row) {
+                freq.put(num, freq.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        // Check numbers from 1 to n^2 to find missing and repeated values
+        for(int num = 1; num <= n * n; num++) {
+            if(!freq.containsKey(num))
+                missing = num; // Number not present in grid
+            else if(freq.get(num) == 2)
+                repeat = num; // Number appears twice
+        }
+        return new int[]{repeat, missing};
+    }
+}
